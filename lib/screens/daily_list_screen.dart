0670fc635d523f7c3f4b3164966164651ec91aad
Sameque Sant' ana <samequesantana@gmail.com>
2025-09-24 13:28:08 -0300

    validation before exit screen register.

--- lib/screens/daily_list_screen.dart
+++ lib/screens/daily_list_screen.dart
@@ -30,33 +30,68 @@ class DailyListScreen extends StatelessWidget {
             itemCount: entryProvider.entries.length,
             itemBuilder: (ctx, i) {
               final entry = entryProvider.entries[i];
-              return Card(
-                margin: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    child: FittedBox(
-                      child: Text(DateFormat('dd/MM').format(entry.date)),
+              return Dismissible(
+                key: Key(entry.id.toString()),
+                direction: DismissDirection.endToStart,
+                confirmDismiss: (direction) async {
+                  return await showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                      title: Text('Tem certeza?'),
+                      content: Text('Deseja apagar este lançamento?'),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.of(ctx).pop(false),
+                          child: Text('Não'),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.of(ctx).pop(true),
+                          child: Text('Sim'),
+                        ),
+                      ],
                     ),
-                  ),
-                  title: Text(
-                    'Lucro: ${currencyFormat.format(entry.netProfit)}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: entry.netProfit >= 0 ? Colors.green : Colors.red,
-                    ),
-                  ),
-                  subtitle: Text(
-                    'Ganhos: ${currencyFormat.format(entry.totalGains)} | Gastos: ${currencyFormat.format(entry.totalExpenses)}',
-                  ),
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (ctx) => AddEntryScreen(
-                          entry: entry,
-                        ), // Passa o 'entry' para a tela de edição
+                  );
+                },
+                onDismissed: (direction) {
+                  entryProvider.deleteEntry(entry.id!);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Lançamento apagado!')),
+                  );
+                },
+                background: Container(
+                  color: Colors.red,
+                  alignment: Alignment.centerRight,
+                  padding: EdgeInsets.only(right: 20.0),
+                  child: Icon(Icons.delete, color: Colors.white),
+                ),
+                child: Card(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      child: FittedBox(
+                        child: Text(DateFormat('dd/MM').format(entry.date)),
                       ),
-                    );
-                  },
+                    ),
+                    title: Text(
+                      'Lucro: ${currencyFormat.format(entry.netProfit)}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: entry.netProfit >= 0 ? Colors.green : Colors.red,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Ganhos: ${currencyFormat.format(entry.totalGains)} | Gastos: ${currencyFormat.format(entry.totalExpenses)}',
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (ctx) => AddEntryScreen(
+                            entry: entry,
+                          ), // Passa o 'entry' para a tela de edição
+                        ),
+                      );
+                    },
+                  ),
                 ),
               );
             },
